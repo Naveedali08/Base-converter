ee31c71487ce33ba3bad30208c6ede643d1c0a8f
Naveed Ali <46053232+Naveedali08@users.noreply.github.com>
2019-04-15 21:02:36 +0500

    Add files via upload

---
+++ ass1/ass1/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace ass1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Hexa = new System.Windows.Forms.RadioButton();
+            this.other = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the number ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(325, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 34);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(408, 338);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 30);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Minion Pro", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(95, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "To binery";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Minion Pro", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(218, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 53);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "To Octal";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Minion Pro", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(362, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 53);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "To hexa";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(255, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 34);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number converter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Minion Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(311, 340);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Minion Pro", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(490, 211);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 53);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "To decimal";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Hexa
+            // 
+            this.Hexa.AutoSize = true;
+            this.Hexa.BackColor = System.Drawing.Color.Transparent;
+            this.Hexa.Location = new System.Drawing.Point(496, 156);
+            this.Hexa.Name = "Hexa";
+            this.Hexa.Size = new System.Drawing.Size(61, 21);
+            this.Hexa.TabIndex = 1;
+            this.Hexa.Text = "Hexa";
+            this.Hexa.UseVisualStyleBackColor = false;
+            this.Hexa.CheckedChanged += new System.EventHandler(this.Hexa_CheckedChanged);
+            // 
+            // other
+            // 
+            this.other.AutoSize = true;
+            this.other.BackColor = System.Drawing.Color.Transparent;
+            this.other.Location = new System.Drawing.Point(496, 183);
+            this.other.Name = "other";
+            this.other.Size = new System.Drawing.Size(79, 21);
+            this.other.TabIndex = 2;
+            this.other.Text = "Decimal";
+            this.other.UseVisualStyleBackColor = false;
+            this.other.CheckedChanged += new System.EventHandler(this.other_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Location = new System.Drawing.Point(581, 156);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 21);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.Text = "Binery";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Location = new System.Drawing.Point(581, 183);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 21);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "Octal";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(791, 484);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.other);
+            this.Controls.Add(this.Hexa);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton Hexa;
+        private System.Windows.Forms.RadioButton other;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ ass1/ass1/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ass1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Hexa.Checked == true)
+            {
+                textBox2.Text = Convert.ToString(Convert.ToInt32(textBox1.Text.ToString(), 16), 2);
+                
+               
+            }
+            else if (other.Checked == true)
+            {
+
+                int number = int.Parse(textBox1.Text);
+                string B = Convert.ToString(number, 2);
+                textBox2.Text = B;
+            }
+
+            else if (radioButton2.Checked == true)
+            {
+                int number = int.Parse(textBox1.Text);
+                string B = Convert.ToString(Convert.ToInt32(textBox1.Text),2);
+                
+                
+                textBox2.Text = B;
+
+            }
+
+            else if (radioButton1.Checked == true)
+            {
+                textBox2.Text = textBox1.Text;
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Hexa.Checked == true)
+            {
+                textBox2.Text = Convert.ToString(Convert.ToInt32(textBox1.Text, 16), 8);
+
+            }
+
+            else if (other.Checked == true)
+            {
+                textBox2.Text = Convert.ToString(Convert.ToInt32(textBox1.Text, 8));
+
+            }
+            else if (radioButton1.Checked == true)
+            {
+                int number = int.Parse(textBox1.Text);
+                string B = Convert.ToString(number, 8);
+                textBox2.Text = B;
+
+            }
+
+            else if (radioButton2.Checked == true)
+            {
+                textBox2.Text = textBox1.Text;
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (Hexa.Checked == true)
+            {
+                textBox2.Text = textBox1.Text;
+
+            }
+            else if (radioButton1.Checked == true)
+            {
+                int number = int.Parse(textBox1.Text);
+                string B = Convert.ToString(number, 16);
+                textBox2.Text = B;
+
+            }
+
+            else if (other.Checked == true)
+            {
+                textBox2.Text = Convert.ToString(Convert.ToInt32(textBox1.Text, 16));
+            }
+            else if (radioButton2.Checked == true)
+            {
+                textBox2.Text = Convert.ToString(Convert.ToInt16(textBox1.Text, 16));
+
+            }
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (Hexa.Checked == true)
+            {
+                textBox2.Text = Convert.ToString(Convert.ToInt32(textBox1.Text, 16));
+            }
+            else if (other.Checked == true)
+            {
+                textBox2.Text = textBox1.Text;
+
+            }
+            else if (radioButton1.Checked == true)
+            {
+               textBox2.Text= Convert.ToString(Convert.ToInt32(textBox1.Text, 2));
+
+            }
+            else if (radioButton2.Checked == true)
+            {
+                if (int.Parse(textBox1.Text) <= 8)
+                {
+                    textBox2.Text = textBox1.Text;
+                }
+
+                else
+                {
+                    textBox2.Text = Convert.ToString(Convert.ToInt32(textBox1.Text, 8));
+                }
+            }
+
+
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hexa_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void other_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
